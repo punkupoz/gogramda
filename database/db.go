67d6d0c4fdb5e4d5911dd62bb979e731b2c1abeb
punkupoz <punkupoz@gmail.com>
2019-07-20 07:33:53 +1000

    database template

--- database/db.go
+++ database/db.go
@@ -1,21 +1,10 @@
 package database
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/jinzhu/gorm"
 
 	// nolint: gotype
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-)
-
-var (
-	host   = os.Getenv("DB_HOST")
-	port   = os.Getenv("DB_PORT")
-	user   = os.Getenv("DB_USER")
-	dbname = os.Getenv("DB_NAME")
-	pass   = os.Getenv("DB_PASS")
 )
 
 // DB is the DB that will performs all operation
@@ -23,10 +12,8 @@ type DB struct {
 	DB *gorm.DB
 }
 
-// NewDB returns a new DB connection
-func newDB(path string) (*DB, error) {
-	// open a new connection
-	conn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", host, port, user, dbname, pass)
+// Open returns a DB connection
+func Open(conn string) (*DB, error) {
 	db, err := gorm.Open("postgres", conn)
 	if err != nil {
 		return nil, err
--- resolver/query.go
+++ resolver/query.go
@@ -2,18 +2,41 @@ package resolver
 
 import (
 	"context"
+	"fmt"
+	"os"
 
 	graphql "github.com/graph-gophers/graphql-go"
+	"github.com/punkupoz/gogramda/database"
 )
 
-type QueryResolver struct{}
+var (
+	host   = os.Getenv("DB_HOST")
+	port   = os.Getenv("DB_PORT")
+	dbuser = os.Getenv("DB_USER")
+	dbname = os.Getenv("DB_NAME")
+	pass   = os.Getenv("DB_PASS")
+)
 
-func NewRoot() (*QueryResolver, error) {
-	return &QueryResolver{}, nil
+// Resolver : connection
+type Resolver struct {
+	db *database.DB
+}
+
+// NewRoot : create a resolver root
+func NewRoot() (*Resolver, error) {
+	conn := fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s", host, port, dbuser, dbname, pass)
+	db, err := database.Open(conn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.DB.AutoMigrate(&User{})
+
+	return &Resolver{db}, nil
 }
 
 // User : Resolver function for the "User" query
-func (r *QueryResolver) User(ctx context.Context, args struct{ ID graphql.ID }) *UserResolver {
+func (r *Resolver) User(ctx context.Context, args struct{ ID graphql.ID }) *UserResolver {
 	p := &User{
 		ID:        "1001",
 		FirstName: "John",
